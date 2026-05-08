5aebde34348151f31d26a78c0210224f8e5dd308
Nilesh Sehrawat <nilesh.sehrawat16@gmail.com>
2026-05-09 01:12:17 +0530

    optimize renderer memory allocation and benchmark warmup

--- src/benchmark.ts
+++ src/benchmark.ts
@@ -1,18 +1,24 @@
 import { render } from "./pool.ts";
 
 async function benchmark() {
-  const JOBS = 20;
+  const JOBS = 100;
 
   const typst = `
-  = Fluxprint Benchmark
-  Hello PDF
+    = Fluxprint Benchmark
+    Hello PDF
   `;
 
+  //warm up benchmark
+  console.log("Warming up...");
+  await Promise.all(Array.from({ length: 20 }, () => render(typst)));
+
+  //actual benchmark
+  console.log("Running benchmark...");
+
   const start = performance.now();
-
   const jobs = Array.from({ length: JOBS }, () => render(typst));
-  await Promise.all(jobs);
 
+  await Promise.all(jobs);
   const end = performance.now();
   const seconds = (end - start) / 1000;
 
